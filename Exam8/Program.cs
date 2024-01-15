5ceff03c3963ca241e46a267ae4cc62993711c3c
Oix135 <oix@mail.ru>
2024-01-15 14:21:02 +0300

    read binary file

--- Exam8/Program.cs
+++ Exam8/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Exam8
+﻿
+namespace Exam8
 {
     internal class Program
     {
+        const string SettingsFileName = @"E:\Oix\Projects\Vent\BinaryFile.bin";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ReadValues();
+
+            Console.ReadKey();
+        }
+
+        private static void ReadValues()
+        {
+            float FloatValue;
+            string StringValue;
+            int IntValue;
+            bool BooleanValue;
+
+            if (!File.Exists(SettingsFileName)) return;
+            try
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(SettingsFileName, FileMode.Open)))
+                {
+                    StringValue = reader.ReadString();
+                }
+                Console.WriteLine(StringValue);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
